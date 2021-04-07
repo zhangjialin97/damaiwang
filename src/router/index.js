d67f0285d9439bd308a0340f73c98a3da780e10d
wangjunjun <1273403463@qq.com>
2021-04-07 10:34:02 +0800

    wangjunjun4-7

--- src/router/index.js
+++ src/router/index.js
@@ -3,61 +3,90 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/Register',
-    name: 'Register',
-    component: () => import('../views/Register.vue')
-  },
-  {
-    path: '/Login',
-    name: 'login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/manage',
-    name: 'manage',
-    component: () => import('../views/manage.vue'),
-    children: [
-      {
+const routes = [{
         path: '/',
-        name: 'Home',
-        component: () => import('../views/Home.vue'),
-      },
-      {
-        path: '/Home',
-        name: 'Home',
-        component: () => import('../views/Home.vue'),
-      },
-      {
-        path: '/baby',
-        name: 'baby',
-        component: () => import('../views/baby.vue'),
-      },
-      // {
-      //   path: '/setInfo2',
-      //   name: 'setInfo2',
-      //   component: () => import('../components/setInfo2.vue'),
-      // },
-      // {
-      //   path: '/iconTable',
-      //   name: 'iconTable',
-      //   component: () => import('../components/iconTable.vue'),
-      // }
-    ]
-  }
+        name: 'login',
+        component: () =>
+            import ('../views/Login.vue')
+    },
+    {
+        path: '/Register',
+        name: 'Register',
+        component: () =>
+            import ('../views/Register.vue')
+    },
+    {
+        path: '/check',
+        name: 'check',
+        component: () =>
+            import ('../views/check.vue')
+    },
+    {
+        path: '/order',
+        name: 'order',
+        component: () =>
+            import ('../views/order.vue')
+    },
+    {
+        path: '/pay',
+        name: 'pay',
+        component: () =>
+            import ('../views/pay.vue')
+    },
+    {
+        path: '/payma',
+        name: 'payma',
+        component: () =>
+            import ('../views/payma.vue')
+    },
+    {
+        path: '/Login',
+        name: 'login',
+        component: () =>
+            import ('../views/Login.vue')
+    },
+    {
+        path: '/manage',
+        name: 'manage',
+        component: () =>
+            import ('../views/manage.vue'),
+        children: [{
+                path: '/',
+                name: 'Home',
+                component: () =>
+                    import ('../views/Home.vue'),
+            },
+            {
+                path: '/Home',
+                name: 'Home',
+                component: () =>
+                    import ('../views/Home.vue'),
+            },
+            {
+                path: '/baby',
+                name: 'baby',
+                component: () =>
+                    import ('../views/baby.vue'),
+            },
+            // {
+            //   path: '/setInfo2',
+            //   name: 'setInfo2',
+            //   component: () => import('../components/setInfo2.vue'),
+            // },
+            // {
+            //   path: '/iconTable',
+            //   name: 'iconTable',
+            //   component: () => import('../components/iconTable.vue'),
+            // }
+        ]
+    }
 
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
