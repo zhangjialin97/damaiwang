147249f34ca8fb58bf450a0958df95dbd0094a0f
wangjunjun <1273403463@qq.com>
2021-04-08 17:20:47 +0800

    wangjunjun4-8-17-25

--- src/components/baby_data.vue
+++ src/components/baby_data.vue
@@ -3,31 +3,51 @@
     <el-row :gutter="20">
       <el-col :span="3"><div class="grid-content bg-purple"></div></el-col>
       <el-col :span="18">
-        <div class="grid-content bg-purple layoutwidth">
+        <div class="grid-content bg-purple layoutWidth">
           <!-- 轮播组件 -->
-          <banner :banners="imgarr"></banner>
+          <div class="layoutWidth">
+            <banner :banners="imgarr"></banner>
+          </div>
+
           <!-- <slot name="banner"></slot> -->
           <!-- 中心内容 -->
           <div class="gurss">
-              <babyGguessVue />
+            <babyGguessVue />
+          </div>
+          <!-- 底部内容 -->
+          <div class="footerTOP">
+            <bottomVue/>
           </div>
         </div>
       </el-col>
       <el-col :span="3"><div class="grid-content bg-purple"></div></el-col>
     </el-row>
+    <div class="footer">
+        <setBottomVue/>
+    </div>
   </div>
 </template>
 <script>
 import banner from "./banner.vue";
-import babyGguessVue from './baby_guess.vue';
+import babyGguessVue from "./baby_guess.vue";
+import bottomVue from './bottom.vue';
+import setBottomVue from './setBottom.vue';
+
 export default {
   components: {
     banner,
-    babyGguessVue
+    babyGguessVue,
+    bottomVue,
+    setBottomVue
   },
   data() {
     return {
-      imgarr: ["/img/qinzi/img_03.png",'/img/base/pic1.jpg','/img/base/pic2.jpg','/img/base/pic3.jpg'],
+      imgarr: [
+        "/img/qinzi/img_03.png",
+        "/img/base/pic1.jpg",
+        "/img/base/pic2.jpg",
+        "/img/base/pic3.jpg",
+      ],
     };
   },
 };
